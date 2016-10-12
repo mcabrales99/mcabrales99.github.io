e9a9d27dc556c9cdfe1d8b8cce130c1b2a4430c1
maurocabrales <mcabrales99@gmail.com>
2016-10-12 14:53:18 -0500

    git new folder views

---
+++ cavazos/views/about.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+
+<html>
+<head>
+    <title>About Us</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    
+</head>
+<body>
+<?php require_once 'navbar.php';?>
+<?php require_once 'header.php';?>
+<style type="text/css">
+  
+p.title{
+
+  font-size: 20px;
+}
+img.fampic {
+  border-style: double;
+  width: 100%;
+}
+
+</style>
+
+
+<div class="container" >
+
+<p class="title">Cavazos Catering has been a small family business owned and operated by Mauro Cabrales.  With over 30 years of experience we strive to make your catering a pleasent experience and will go above and beyond to make your day of celebration amazing.</p>
+<br>
+<img class="fampic" src="img/fam.jpg" alt="">
+
+ </div>
+<?php require_once 'footer.php';?>
+</body>
+</html>
+
+
