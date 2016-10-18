0c9218345ba5f3261be91456fea4edbb1cf1aa48
maurocabrales <mcabrales99@gmail.com>
2016-10-17 23:35:57 -0500

    these are the files that are going to be erased

--- cavazos/navbar.php
+++ cavazos/navbar.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="keywords" content="catering, cavazos, food, meals, rgv, texas, mcallen, mission, donna, alamo, rio grande valley, weslaco, valley, ">
+  <meta name="keywords" content="catering, cavazos, food, meals, rgv, texas, mcallen, mission, donna, alamo, rio grande valley, weslaco, valley">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
